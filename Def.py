2aef8a8c865fdca6a58d6b5b7646eec5f53333e1
TOD24 <zloigenii42@gmail.com>
2023-10-10 11:30:53 +0300

    Добавленна функция управления питанием компютера

--- Def.py
+++ Def.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 
 def Programs(file_puth):  # функция запуска программ по пути
@@ -7,3 +8,15 @@ def Programs(file_puth):  # функция запуска программ по 
         print(f"Программа {file_puth} запущенна")
     except:
         print("ошибка при запуске программы")
+
+
+def sustem(comand):
+    try:
+        if "off" in comand:
+            os.system("shutdown /s /t 0")
+        elif "reboot" in comand:
+            os.system("shutdown /r /t 0")
+        elif "h" in comand:
+            os.system("shutdown /h /t 0")
+    except Ellipsis as e:
+        print(f"Произошла ошибка: {e}")
