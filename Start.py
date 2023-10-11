e200bba3d21fcb36520a89c8c13dec1045a1f6fe
TOD24 <zloigenii42@gmail.com>
2023-10-11 16:38:59 +0300

    сохранил изменения в файле Start

--- Start.py
+++ Start.py
@@ -5,32 +5,3 @@ from Def import sp
 
 t = sp("Как вас зовут: ")
 print(f"Вас зовут: {t}")
-
-# text = input("Введите команду: ")
-# sp = text.split()
-# print("------------------------------------------")
-# print(sp)
-# print("------------------------------------------")
-# 
-#     
-# fi = ["найди", "запусти", "папке"]
-# ind = []
-# for s in fi:
-#     try:
-#         ind.append(sp.index(s))
-#         print(sp.index(s))
-#         print(sp[sp.index(s):])
-#     except:
-#         print(f"Индекст '{s}' не найден")
-# 
-# print(f"ind: {ind}")
-# ind2 = sorted(ind)
-# print(f"ind2: {ind2}")
-# for i, s in enumerate(ind2):
-#     try:
-#         i1 = ind2[i] + 1
-#         i2 = ind2[i+1]
-#         print(sp[i1:i2])
-#     except:
-#         i1 = ind2[i] + 1
-#         print(sp[i1:])       
